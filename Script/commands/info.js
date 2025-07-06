c78af1147b52ad7ba341cb36d26b3e526392fd24
saeid12321 <emoji123tyj@gmail.com>
2025-07-06 21:40:40 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "info",
  version: "1.2.6",
  hasPermssion: 0,
- credits: "Shaon Ahmed",
+ credits: "Saeid Boss",
  description: "info bot owner",
  commandCategory: "For users",
  hide:true,
