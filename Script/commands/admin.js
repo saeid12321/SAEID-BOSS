bf44e0e64dc6d88f2c437973da7459bb5cc4ad77
saeid12321 <emoji123tyj@gmail.com>
2025-07-07 03:12:10 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -4,10 +4,10 @@ const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
 module.exports.config = {
-    name: "SAEID",
+    name: "admin",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "SAEID", //don't change my credit 
+    credits: "ULLASH", //don't change my credit 
     description: "Show Owner Info",
     commandCategory: "info",
     usages: "",
@@ -22,17 +22,17 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━━━━━━┓
 ┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐒𝐀𝐄𝐈𝐃\\____😘😈🩵🪽
+┃ 👤 𝐍𝐚𝐦𝐞      : 𝐒𝐀𝐄𝐈𝐃 \\____😘😈🩵🪽
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
 ┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : Single 
 ┃ 🎂 𝐀𝐠𝐞       : 19
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
 ┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Rajshahi, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Rajshahi, Bangladesh 
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : saeidking2256
-┃ 
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/profile.php?id=122141837090449039
+┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : saeid
+┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : nai 😆
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/profile.php?id=122096166764085465
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
@@ -40,7 +40,7 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    return request(encodeURI(`https://graph.facebook.com/122096166764085465/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
